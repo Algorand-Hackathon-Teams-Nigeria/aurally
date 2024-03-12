ce4baa9efb0871258eb913955dee6cfd3423414d
Godswill <godswillanwuli@gmail.com>
2024-03-12 13:12:05 +0100

    fixing seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,19 +54,6 @@ export const metadata: Metadata = {
     description: details.description,
     images: details.image,
   },
-  robots: {
-    index: false,
-    follow: true,
-    nocache: true,
-    googleBot: {
-      index: true,
-      follow: false,
-      noimageindex: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
-    },
-  } as const,
   keywords: [
     "NFT",
     "nft",
