6e91ff7d5d8b940b9f6b2341f5c4e1ac111bfe70
DexFA <110574901+DexFA@users.noreply.github.com>
2025-02-04 19:57:20 +0100

    Landing responsive adjustment for homepage

--- src/app/components/page-sections/landing/new-fans/pl-for-fans copy/index.tsx
+++ src/app/components/page-sections/landing/new-fans/pl-for-fans copy/index.tsx
@@ -1,73 +1,80 @@
 "use client";
 
-import Head from 'next/head';
-import React from 'react';
-import Image from 'next/image';
-import EmblaCarousel from '@ui/EmblaCarousel';
+import EmblaCarousel from "@ui/EmblaCarousel";
+import Head from "next/head";
+import Image from "next/image";
 
 const HeroText = ({
-  mainTitle = "Discover, Stream and collect the Rarest NFTs",
-  subtitle = "Connect Directly with your favourite artists"
+	mainTitle = "Discover, Stream and collect the Rarest NFTs",
+	subtitle = "Connect Directly with your favourite artists",
 }) => (
-  <div className="hero-text-container sm:ml-40 text-center sm:text-left lg:translate-y-[100px] mx-auto">
-    <h1
-      className="leading-[110%] font-bold text-4xl sm:text-4xl max-w-[635px] mx-auto sm:mx-0 sm:mt-[-330px] translate-y-[20px] lg:translate-x-0 translate-x-[10px]"
-    >
-      <span className="">Discover, </span>
-      <span className=" text-transparent bg-clip-text bg-gradient-to-r from-[#E22BCC] to-[#FBB03B]">
-        Stream
-      </span>
-      <span className="block sm:inline"> and {" "}collect the <span className=" block sm:inline text-transparent bg-clip-text bg-gradient-to-r from-[#E22BCC] to-[#FBB03B]">
-        Rarest NFTs
-      </span>
-      </span>
-    </h1>
-    <p className="hero-subtitle mt-2 mx-auto sm:mx-0 translate-y-[20px] lg:translate-x-0 translate-x-[10px]">{subtitle}</p>
-  </div>
+	<div className="hero-text-container sm:ml-40 text-center sm:text-left lg:translate-y-[100px] mx-auto">
+		<h1 className="leading-[110%] font-bold text-4xl sm:text-4xl max-w-[635px] mx-auto sm:mx-0 sm:mt-[-330px] translate-y-[20px] lg:translate-x-0 translate-x-[10px]">
+			<span className="">Discover, </span>
+			<span className=" text-transparent bg-clip-text bg-gradient-to-r from-[#E22BCC] to-[#FBB03B]">
+				Stream
+			</span>
+			<span className="block sm:inline">
+				{" "}
+				and collect the{" "}
+				<span className=" block sm:inline text-transparent bg-clip-text bg-gradient-to-r from-[#E22BCC] to-[#FBB03B]">
+					Rarest NFTs
+				</span>
+			</span>
+		</h1>
+		<p className="hero-subtitle mt-2 mx-auto sm:mx-0 translate-y-[20px] lg:translate-x-0 translate-x-[10px]">
+			{subtitle}
+		</p>
+	</div>
 );
 
 const HeroButtons = ({
-  onGetStarted = () => console.log('Get Started clicked'),
-  onExplore = () => console.log('Explore clicked'),
+	onGetStarted = () => console.log("Get Started clicked"),
+	onExplore = () => console.log("Explore clicked"),
 }) => (
-  <div className="flex flex-col gap-2.5 mt-8 sm:ml-40 ml-0 sm:mr-0 mr-4 w-full max-w-[500px] mx-auto translate-y-[20px]">
-    <div className="flex gap-2.5 justify-center">
-      <button
-        className="flex items-center justify-center px-4 py-3 bg-[#8a2be2] text-white text-base font-bold rounded-full min-w-[128px] transition-all duration-300 ease-in-out hover:bg-[#7825c2]"
-        onClick={onGetStarted}
-      >
-        <span>Get Started</span>
-      </button>
-      <button
-        className="flex items-center justify-center px-4 py-3 border border-[#c6c6c6] text-[#c6c6c6] text-base font-bold rounded-full min-w-[121px] transition-all duration-300 ease-in-out hover:border-[#8a2be2] hover:text-[#8a2be2]"
-        onClick={() => window.location.href = 'https://app.aurally.xyz/explore'}
-      >
-        <span>Explore</span>
-      </button>
-    </div>
-  </div>
+	<div className="flex flex-col gap-2.5 mt-8 sm:ml-40 ml-0 sm:mr-0 mr-4 w-full max-w-[500px] mx-auto translate-y-[20px]">
+		<div className="flex gap-2.5 justify-center">
+			<button
+				className="flex items-center justify-center px-4 py-3 bg-[#8a2be2] text-white text-base font-bold rounded-full min-w-[128px] transition-all duration-300 ease-in-out hover:bg-[#7825c2]"
+				onClick={onGetStarted}
+			>
+				<span>Get Started</span>
+			</button>
+			<button
+				className="flex items-center justify-center px-4 py-3 border border-[#c6c6c6] text-[#c6c6c6] text-base font-bold rounded-full min-w-[121px] transition-all duration-300 ease-in-out hover:border-[#8a2be2] hover:text-[#8a2be2]"
+				onClick={() =>
+					(window.location.href = "https://app.aurally.xyz/explore")
+				}
+			>
+				<span>Explore</span>
+			</button>
+		</div>
+	</div>
 );
 
 const HomePage = () => (
-  <div>
-    <Head>
-      <title>Aurally - Discover, Stream, Collect NFTs</title>
-      <meta name="description" content="Connect Directly with your favorite artists." />
-    </Head>
-    <main
-      className="grid grid-cols-12 gap-8 px-4 pt-16 sm:pt-[200px] bg-cover bg-center min-h-screen sm:min-h-[calc(100vh+100px)]"
-      style={{ backgroundImage: "url('/Black & White 1 1.png')" }}
-    >
-      {/* Left Column - HeroText */}
-      <div className="col-span-12 sm:col-span-6 flex flex-col justify-center items-start">
-        <HeroText />
+	<div className="h-screen">
+		<Head>
+			<title>Aurally - Discover, Stream, Collect NFTs</title>
+			<meta
+				name="description"
+				content="Connect Directly with your favorite artists."
+			/>
+		</Head>
+		<main
+			className="grid grid-cols-12 gap-8 px-4 pt-16 sm:pt-[200px] bg-cover bg-center h-screen sm:min-h-[calc(100vh+100px)]"
+			style={{ backgroundImage: "url('/Black & White 1 1.png')" }}
+		>
+			{/* Left Column - HeroText */}
+			<div className="col-span-12 sm:col-span-6 flex flex-col justify-center items-start">
+				<HeroText />
 
-        <div className="flex sm:hidden mt-4 w-full">
-          <HeroButtons />
-        </div>
-      </div>
+				<div className="flex sm:hidden mt-4 w-full">
+					<HeroButtons />
+				</div>
+			</div>
 
-      {/* Right Column - Phone Image */}
+			{/* Right Column - Phone Image */}
 			<div
 				className="col-span-12 sm:col-span-6 flex justify-center items-center sm:mt-[70px] mt-[10px] sm:mb-16 relative"
 				style={{
@@ -78,7 +85,6 @@ const HomePage = () => (
 					height: "100%",
 				}}
 			>
-				
 				<div
 					className="sm:hidden absolute inset-0"
 					style={{
@@ -86,6 +92,7 @@ const HomePage = () => (
 						backgroundSize: "cover",
 						backgroundPosition: "center",
 						zIndex: -1,
+						height: "100%",
 					}}
 				/>
 				{/* Phone Mockup Image */}
@@ -94,14 +101,17 @@ const HomePage = () => (
 					alt="Phone Mockup"
 					width={300}
 					height={600}
-					className="w-80 md:w-96 lg:w-full max-h-[65vh] object-contain z-10"
+					className="w-80 md:w-96 lg:w-full max-h-[55vh] object-contain z-10"
 				/>
 			</div>
 
-     {/* Embla Carousel and Buttons for Desktop */}
+			{/* Embla Carousel and Buttons for Desktop */}
 			<div className="col-span-12 flex flex-col justify-center items-start sm:mt-8 ">
-				<div className="flex flex-row gap-6 mt-[-530px] sm:mt-[-770px]">
-					<div key="carousel" className="flex flex-col gap-4 w-full opacity-30 lg:mb-[-530px]">
+				<div className="flex flex-row gap-6 mt-[-530px] sm:mt-[-1170px]">
+					<div
+						key="carousel"
+						className="flex flex-col gap-4 w-full opacity-30 lg:mb-[-530px]"
+					>
 						<EmblaCarousel />
 					</div>
 				</div>
