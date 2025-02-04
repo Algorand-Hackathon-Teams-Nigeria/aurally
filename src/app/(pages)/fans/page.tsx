3cedc65144d1f24953968dad78f998bf4d5e81d4
emmylem <benjaminosondu55@gmail.com>
2025-02-04 18:56:34 +0100

    mobile update

--- src/app/(pages)/fans/page.tsx
+++ src/app/(pages)/fans/page.tsx
@@ -10,19 +10,25 @@ export const metadata: Metadata = {
 const FansPage = () => {
   return (
     <Page className="mt-0 px-0 lg:px-0 p-0 lg:py-0 gap-0 lg:gap-0">
-      <div className="lg:mt-[0px] mt-[0px]"> 
+      <div className="flex flex-col items-center lg:mt-[0px] mt-[120px]"> 
         <Landing.HomePage />
         <Landing.AppStatistics />
-        
-        {/* Adjusting the container to allow overflow */}
-        <div className="w-full h-auto mx-auto space-y-20 sm:space-y-32 md:space-y-36">
+
+        {/* Adjusting the container to allow stacking on mobile */}
+        <div className="w-full h-auto mx-auto flex flex-col space-y-16 sm:space-y-32 md:space-y-36">
           <div className="max-w-screen-lg mx-auto pt-[20px]">
             <Landing.AboutUsNew />
           </div>
-          <Landing.LandingSection />
+          
+          {/* Ensures LandingSection moves down properly */}
+          <div className="w-full flex justify-center sm:mt-[0px] mt-[40px]">
+            <Landing.LandingSection />
+          </div>
+
           <div className="max-w-screen-lg mx-auto pt-[20px]">
             <Landing.ForCreatorsNew />
           </div>
+          
           <Landing.SupportText />
           <Landing.How_it_works />
           <Landing.NewCommunity />
