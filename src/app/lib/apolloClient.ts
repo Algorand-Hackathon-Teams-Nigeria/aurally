b2547c51a279c7c68e6aa7ded793e067a6018d34
emmylem <benjaminosondu55@gmail.com>
2025-03-31 09:04:58 +0100

    update

--- src/app/lib/apolloClient.ts
+++ src/app/lib/apolloClient.ts
@@ -1,29 +1,29 @@
-// import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
-// import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rsc";
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rsc";
 
-// export const { getClient } = registerApolloClient(() => {
-//   return new ApolloClient({
-//     cache: new InMemoryCache(),
-//     link: new HttpLink({
-//       uri: "https://aurallybackend-production.up.railway.app/graphql",
-//     }),
-//   });
-// });
-
-import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
-
-const API_BASE_URL =
-  process.env.NODE_ENV === "development"
-    // ? "https://devapi.aurally.xyz/graphql"
-    ? "http://localhost:8000/graphql"
-    : "https://aurallybackend-production.up.railway.app/graphql";
-
-const client = new ApolloClient({
-  link: new HttpLink({
-    uri: "https://aurallybackend-production.up.railway.app/graphql",
-    credentials: "include", // Ensures cookies and authentication work
-  }),
-  cache: new InMemoryCache(),
+export const { getClient } = registerApolloClient(() => {
+  return new ApolloClient({
+    cache: new InMemoryCache(),
+    link: new HttpLink({
+      uri: "https://aurallybackend-production.up.railway.app/graphql",
+    }),
+  });
 });
 
-export default client;
+// import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
+
+// const API_BASE_URL =
+//   process.env.NODE_ENV === "development"
+//     // ? "https://devapi.aurally.xyz/graphql"
+//     ? "http://localhost:8000/graphql"
+//     : "https://aurallybackend-production.up.railway.app/graphql";
+
+// const client = new ApolloClient({
+//   link: new HttpLink({
+//     uri: "https://aurallybackend-production.up.railway.app/graphql",
+//     credentials: "include", // Ensures cookies and authentication work
+//   }),
+//   cache: new InMemoryCache(),
+// });
+
+// export default client;
