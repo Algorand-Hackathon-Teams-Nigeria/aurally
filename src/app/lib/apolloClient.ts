e38ca766e4a8e9df9917093f0e9fef0f5fd3018e
emmylem <benjaminosondu55@gmail.com>
2025-04-22 14:53:44 +0100

    admin login integrated

--- src/app/lib/apolloClient.ts
+++ src/app/lib/apolloClient.ts
@@ -1,14 +1,14 @@
-import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
-import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rsc";
+// import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+// import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rsc";
 
-export const { getClient } = registerApolloClient(() => {
-  return new ApolloClient({
-    cache: new InMemoryCache(),
-    link: new HttpLink({
-      uri: "https://aurallybackend-production.up.railway.app/graphql",
-    }),
-  });
-});
+// export const { getClient } = registerApolloClient(() => {
+//   return new ApolloClient({
+//     cache: new InMemoryCache(),
+//     link: new HttpLink({
+//       uri: "https://aurallybackend-production.up.railway.app/graphql",
+//     }),
+//   });
+// });
 
 // import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
@@ -43,4 +43,18 @@ export const { getClient } = registerApolloClient(() => {
 // });
 
 // export default client;
+
+// src/app/lib/apolloClient.ts (client-safe version)
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: "https://aurallybackend-production.up.railway.app/graphql",
+    credentials: "include",
+  }),
+  cache: new InMemoryCache(),
+});
+
+export default client;
+
 
